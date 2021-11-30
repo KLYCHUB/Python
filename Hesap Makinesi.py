4318443959a9e2c292080ad9c32e2f9599fdc449
Eren KALAYCI <94598244+KLYCHUB@users.noreply.github.com>
2021-12-01 01:35:43 +0300

    Create Hesap Makinesi.py

---
+++ Hesap Makinesi.py
@@ -0,0 +1,14 @@
+hesap_makinesi = input("""
+
+Hesap Makinesi
+
+Toplama = +
+cikarma = -
+Bolme = /
+Carpma = *
+
+NOT: isleminizi 45*32 seklinde giriniz
+
+İsleminiz: """)
+
+print("Sonucunuz:",eval(hesap_makinesi))
