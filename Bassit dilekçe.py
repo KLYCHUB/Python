e32d8bc5c63c92b29b05a418f75dcc509973719a
Eren KALAYCI <94598244+KLYCHUB@users.noreply.github.com>
2021-12-01 00:42:27 +0300

    Create Bassit dilekçe.py

---
+++ Bassit dilekçe.py
@@ -0,0 +1,16 @@
+dilekce = ("""
+adiniz: {}
+soyadiniz: {}
+kayit tarihi: {}/{}/{}
+""")
+
+ad = str(input("Adinizi girininiz: "))
+soyad = str(input("Soyadinizi girininiz: "))
+
+gün = int(input("Kayit aldugunuz gunu giriniz: "))
+ay = int(input("Kayit aldugunuz ayi giriniz: "))
+yil = int(input("Kayit aldugunuz yili giriniz: "))
+
+print("\n--------------------------------------\n")
+
+print("Dilekçeniz: ",dilekce.format(ad,soyad,gun,ay,yil))
