c826a4afbabfe3ca48a07fc0021e943ba689968f
Eren KALAYCI <94598244+KLYCHUB@users.noreply.github.com>
2021-12-01 01:27:37 +0300

    Create Fatura hesaplama.py

---
+++ Fatura hesaplama.py
@@ -0,0 +1,8 @@
+x = str(input("Faturasini odemek istediginiz ayi yaziniz: "))
+y = int(input("Kac metre dogalgaz kullandiginizi yaziniz: "))
+
+fatura = y*1.54
+
+print(x,"Ayinin faturasi",fatura,"TL")
+
+#Dogalgazın metreküp fiyat15ı 1.54 TL olsun
